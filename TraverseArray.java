5792acc8c817297c256e17e02972e05e9d03c3dc
subhiksha2611 <60611731+subhiksha2611@users.noreply.github.com>
2021-09-24 23:44:26 +0530

    Add files via upload

---
+++ TraverseArray.java
@@ -0,0 +1,15 @@
+package com.ebox;
+
+public class TraverseArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr=new int[] {11,67,45,89,20,76};
+		for(int i:arr) //for(type variable name:array name)
+		{
+			System.out.println(i);
+		}
+
+	}
+
+}
