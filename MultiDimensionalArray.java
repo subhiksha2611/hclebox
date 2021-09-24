df9d91099ce6808739b53388306ef538742da3e1
subhiksha2611 <60611731+subhiksha2611@users.noreply.github.com>
2021-09-24 23:27:43 +0530

    Add files via upload

---
+++ MultiDimensionalArray.java
@@ -0,0 +1,21 @@
+package com.ebox;
+
+public class MultiDimensionalArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[][]={{14,34,72,65},{32,78,54,90}};
+		for(int row=0;row<4;row++)
+		{
+			for(int col=0;col<4;col++)
+			{
+				System.out.print(arr[row][col]+" ");
+				
+			}
+			System.out.println("");
+	    }
+
+
+	 }
+	
+}
