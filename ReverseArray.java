e19fabcac776feb12df237f47915e81b70bd56fa
subhiksha2611 <60611731+subhiksha2611@users.noreply.github.com>
2021-09-24 23:30:30 +0530

    Add files via upload

---
+++ ReverseArray.java
@@ -0,0 +1,14 @@
+package com.ebox;
+
+public class ReverseArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr=new int[] {12,13,14,15,16,17};
+		for(int i=arr.length-1;i>=0;i--)
+		{
+		System.out.println("Reverse Array : "+arr[i]);
+		}
+	}
+
+}
